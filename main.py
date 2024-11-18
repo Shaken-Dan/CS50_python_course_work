a1f5daa958f36592fab4dc843eaeda6bc36940c4
“ShD <bolatuly.shaken@gmail.com>
2024-11-18 16:32:18 +0500

    fzm update

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, CallbackQuery
 from aiogram import F
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
 from schedule import schedule_data
 import asyncio
 import openpyxl
@@ -13,12 +16,34 @@ dp = Dispatcher()
 
 user_class = {}
 
+class MyCallback(CallbackData, prefix="my"):
+    foo: str
+    bar: int
+
+def create_keyboard():
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text="demo",
+        callback_data=MyCallback(foo="demo", bar=42)
+        # Value can be not packed to string inplace, because builder knows what to do with callback instance
+    )
+    builder.button(
+        text="demo2",
+        callback_data=MyCallback(foo="demo2", bar=42)
+        # Value can be not packed to string inplace, because builder knows what to do with callback instance
+    )
+
+    return builder.as_markup()
+
+# Filter callback by type and value of field :code:`foo`
+@dp.callback_query(MyCallback.filter(F.foo == "demo"))
+async def my_callback_foo(query: CallbackQuery, callback_data: MyCallback):
+    await query.message.answer(f"button pressed {callback_data}")
 
 # Welcome message handler
 @dp.message(Command(commands=['start']))
 async def send_welcome(message: Message):
-    await message.answer("Hello! Please enter your class: ")
-
+    await message.answer("Hello! Please enter your class: ", reply_markup=create_keyboard())
 
 # Handle class input
 @dp.message(F.text)
